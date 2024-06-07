bbd7056cdb4a73abecfbb7b731dab999cc9a8de5
resfer3 <resfer333@gmail.com>
2024-06-07 05:52:39 -0600

    Found a way to print stdin, just have to implement it right

--- cccat.c
+++ cccat.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
 
+#define BUFFERSIZE 128
+
+int isBlank (char *line);
 
 int main(int argc, char *argv[]){
 
@@ -9,32 +14,104 @@ int main(int argc, char *argv[]){
         printf("Usage: ./cccat file\n"); 
         return 1;
     }
-   /* if (argv[1]){
+   
 
-        printf("%s\n", argv[1]);
-    }*/
+    FILE *input;
+    /*
+    (if (argv[1] == "-"){
+       input = popen("!!", "r");
+       if (input == NULL){
+            printf("stdin Not Found\n");
+            return 1;
+       } else {
 
-
-    //open file from command-line argument
-    FILE *file = fopen(argv[1], "r"); 
-    if (file == NULL){
-    //if file is null, read from standard in
-        printf("Couldn't find stdin\n");
-        return 1;
+            while(fgets(path, sizeof(path), input) != NULL){
+                printf("%s", path);
+            }
+       }
+    }
+    else {
+        input = fopen(argv[1], "r");
+        if (input == NULL){
+        //if file is null, read from standard in
+            printf("Couldn't find file\n");
+            return 1;
+        }    
+    }
+    */
+    // if argv[1] is file; declare file
+    // else argv[1] is -; declare stdin
+    if (argv[1] == "-"){
+        
+    }
+    else{
+        input = fopen(argv[1], "r");
     }
 
+
+    //NOTE TO SELF: 07/06
+    //MAKE THE BELOW CODE TO A FUNCTION TO WRITE TO STDOUT
+    //SO IT WILL BE ABLE TO GET MORE DIVERSE OUTPUTS
+
+
+
+
+    
     //write contents to stdout
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
-    while ((read = getline(&line, &len, file))!= -1){
+    while ((read = getline(&line, &len, input))!= -1){
     
         //printf("Success\n");
         printf("line of length %zu : \n", read);
         printf("%s", line);
 
     }
-    free(file);
-    
+    fclose(input);
+    return 0;    
 
 }
+
+/*
+// not needed
+// return nonzero if line is a string containing only whitespaces, -1 otherwise
+int isBlank (char *line){
+    char *tmp;
+    int is_blank = -1;
+    // Loop through each character
+    for (tmp = line; *tmp != '\0'; ++tmp){
+        if(!isspace(*tmp)){
+        // Found a non-whitespace character
+        is_blank = 0;
+        break;
+        }
+    }
+
+    return is_blank;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
