fc4e3f53f7bec7ea8a9aa7c43c36898490cf6187
resfer3 <resfer333@gmail.com>
2024-06-07 20:05:42 -0600

    Added concatenate files

--- cccat.c
+++ cccat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 #define BUFFERSIZE 128
 
@@ -10,12 +11,19 @@ int printFile(char *filename, FILE** read_file);
 int main(int argc, char *argv[]){
 
     //err handling
-    if (argc != 2){
+    if (argc > 3){
         printf("Usage: ./cccat file\n"); 
         return 1;
     }
    
     FILE *input;
+    FILE *input2;
+    char data[argc + 1][100];
+    // loop into string
+    for (int i = 1; i < argc; i++){
+        strcpy(data[i], argv[i]);
+    }
+    
 
     //NOTE TO SELF: 07/06
     //MAKE THE BELOW CODE TO A FUNCTION TO WRITE TO STDOUT
@@ -25,15 +33,24 @@ int main(int argc, char *argv[]){
     size_t len2 = 0;
     ssize_t read;
     
-    printFile(argv[1], &input);
 
-    while ((read = getline(&std, &len2, stdin)) != -1){
-        printf("%s", std);
+    if (strcmp(data[1], "-") == 0){
+
+        while ((read = getline(&std, &len2, stdin)) != -1){
+            printf("%s", std);
+        }
+        return 0;
+    }
+    else if (data[1] != NULL || data[2] != NULL){
+
+        printFile(data[1], &input);
+        printFile(data[2], &input2);
+    
     }
     
 
 
-    
+    //free(data); 
     //write contents to stdout
     return 0;    
 
@@ -58,7 +75,6 @@ int printFile(char *filename, FILE **read_file){
     if (*read_file != NULL){
         fclose(*read_file);
     }
-    exit(0);
     return 0;
 }
 
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ Trying to grasp some c here, before jumping to higher level languages...
 
 - Step 1 - Open specified file from command line and print contents to stdout -> :white_check_mark:
 - Step 2 - If no files, read input from stdin and output to stdout -> :white_check_mark:   
-- Step 3 - Concatenate files 
+- Step 3 - Concatenate files -> :white_check_mark: 
 - Step 4 - Number the lines as they're printed out 
 - Step 5 - Number lines, both including and excluding non-blank lines 
 
